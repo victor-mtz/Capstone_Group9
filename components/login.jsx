3c9cbf670d68d37aaaf94cb8c0ec1c25b3ade07e
Victor Martinez <victormartinez@Victors-MacBook-Air.local>
2024-03-20 18:33:17 -0600

    adding redux functionality and storing token in state

--- components/login.jsx
+++ components/login.jsx
@@ -1,74 +1,72 @@
 import React, { useState } from 'react';
+import { useLoginMutation, useRegisterMutation } from '../utils/api/authSlice';
 
-const Login = ({ setToken, navigation }) => {
+const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+  const authType = isLogin ? 'Login' : 'Register';
+  const oppositeAuthCopy = isLogin
+    ? "Don't have an account?"
+    : 'Already have an account?';
+  const oppositeAuthType = isLogin ? 'Register' : 'Login';
+
+  // redux code below
+  const [login] = useLoginMutation();
+  const [register] = useRegisterMutation();
+  // redux code above
 
   const handleAction = async (e) => {
     e.preventDefault();
     setError('');
+    const authMethod = isLogin ? login : register;
 
     try {
-      // const apiUrl = action === 'register' ? 'registerUrl' : 'loginUrl';
-      const apiUrl = 'http://localhost:5433/api/users/login';
-
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Invalid Email or Password. Please try again.');
-      }
-
-      const data = await response.json();
+      const data = await authMethod({ username, password }).unwrap();
       console.log(data);
       setSuccessMessage(data.message);
-      setToken(data.token);
     } catch (error) {
       setError(error.message || 'An error occurred. Please try again.');
     }
   };
 
-  // const title = action === 'login' ? 'Login' : 'Signup';
-
   return (
-    <form onSubmit={handleAction} className="container">
-      <div className="header">
-        <div className="text">Nothing</div>
-        <div className="underline"></div>
-      </div>
-      {successMessage && (
-        <div className="success-message">{successMessage}</div>
-      )}
-      <div className="inputs">
-        <div className="input">
-          <input
-            placeholder="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <div>
+      <form onSubmit={handleAction} className="container">
+        <div className="header">
+          <div className="text">{authType}</div>
+          <div className="underline"></div>
         </div>
-        <div className="input">
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        {successMessage && (
+          <div className="success-message">{successMessage}</div>
+        )}
+        <div className="inputs">
+          <div className="input">
+            <input
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
-      {error && <div className="error-message">{error}</div>}
-      <button className="submit">Nothing</button>
-    </form>
+        {error && <div className="error-message">{error}</div>}
+        <button className="submit">{authType}</button>
+      </form>
+      <p>
+        {oppositeAuthCopy}{' '}
+        <a onClick={() => setIsLogin(!isLogin)}>{oppositeAuthType}</a>
+      </p>
+    </div>
   );
 };
 
